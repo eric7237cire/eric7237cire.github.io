f3867d725422ad6950836b792d56a5960a379dc1
eric7 <eric7237cire@hotmail.fr>
2022-05-14 17:45:18 +0200

    push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SpanishRootComponent} from "./spanish-root/spanish-root.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'spanish', component: SpanishRootComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpanishRootComponent } from './spanish-root/spanish-root.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpanishRootComponent
   ],
   imports: [
     BrowserModule,
